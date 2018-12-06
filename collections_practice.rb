9e7985e840eb30b13cdf987b6ff62587deace2b5
Evan Pavley <epavley@sva.edu>
2018-12-06 01:56:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all?{|i| i[0] == "r"}
+end
+
+def contain_a(arr)
+  arr.select{|i| i.include? "a"}
+end
+
+def first_wa(arr)
+  arr.detect{|i| i[0,1] "wa"}
+end
+
+def remove_non_strings
+  
+end
+
+def count_elements
+  
+end
+
+def merge_data
+  
+end
+
+def find_cool
+  
+end
+
+def organize_schools
+  
+end
+
